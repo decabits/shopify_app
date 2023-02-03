dd45c674de308a87aed3e9a9be2606394598f6ce
Sahil <sahilbathla1@gmail.com>
2023-02-03 15:18:21 +0530

    Show continue app button after 5 seconds

--- app/assets/javascripts/shopify_app/post_redirect.js
+++ app/assets/javascripts/shopify_app/post_redirect.js
@@ -4,6 +4,13 @@
     if (form) {
       form.submit();
     }
+    setTimeout(function() {
+      var continueApp = document.getElementById("continue-app");
+      if (continueApp) {
+        // Remove visibility set to use default
+        continueApp.style.visibility = null;
+      }
+    }, 5000)
   }
   document.addEventListener("DOMContentLoaded", redirect);
 })();
